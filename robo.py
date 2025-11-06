6e615f94fae234ffdedfb492494a0e09d12dbfe9
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2025-11-06 19:46:37 -0300

    Add files via upload

---
+++ robo.py
@@ -0,0 +1,26 @@
+"""
+    Robôs Profissionais com Python: Estrutura, Implantação e Monitoramento
+
+    Criar um robô básico, implantá-lo na nuvem dentro do GitHub e executá-lo para ver sua execução diária.
+"""
+
+import datetime
+import pandas as pd
+
+# Simulação de coleta de dados
+def coletar_dados():
+    return [
+        {"data": datetime.date.today(), "evento": "Processamento finalizado", "status": "OK"}
+    ]
+
+# Salvar em um CSV
+def salvar_relatorio(dados):
+    df = pd.DataFrame
+    df.to_csv("dados/relatorio.csv", index=False)
+    print("Relatório salvo com sucesso!")
+
+# Execução principal
+if __name__ == "__main__":
+    print("Iniciando Robô...")
+    dados = coletar_dados()
+    salvar_relatorio(dados)
