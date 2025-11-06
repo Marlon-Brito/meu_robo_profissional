a0cbd39579e024c7e7eeef3a5fbdb6d6d192fbee
Marlon Brito dos Santos Castor <148072815+Marlon-Brito@users.noreply.github.com>
2025-11-06 20:11:35 -0300

    Update robo.py

--- robo.py
+++ robo.py
@@ -6,6 +6,7 @@
 
 import datetime
 import pandas as pd
+import os
 
 # Simulação de coleta de dados
 def coletar_dados():
@@ -15,13 +16,14 @@ def coletar_dados():
 
 # Salvar em um CSV
 def salvar_relatorio(dados):
+    os.makedirs("dados", exist_ok=True) # garante que a pasta existe
     df = pd.DataFrame(dados)
-    df.to_csv("dados/relatorio.csv", index=False)
+    print("DataFrame gerado:\n", df)
+    df.to_csv("dados/relatorio.csv", index=False, encoding="utf-8")
     print("Relatório salvo com sucesso!")
 
 # Execução principal
 if __name__ == "__main__":
     print("Iniciando Robô...")
     dados = coletar_dados()
-
     salvar_relatorio(dados)
